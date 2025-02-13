dc33482885de7460f3628bd00b23562d8099e872
Wojtyla <144253449+RaplesWojtyla@users.noreply.github.com>
2025-02-13 08:50:18 +0700

    feat: add create post section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import CreatePost from "@/components/CreatePost"
+
 const Home = () => {
 	return (
-		<div className='m-4'>
-			Main Content
+		<div className='grid grid-cols-1 lg:grid-cols-10 gap-6'>
+			<div className="lg:col-span-6">
+				<CreatePost />
+			</div>
+			<div className="hidden lg:block lg:col-span-4 sticky top-20">
+				Follow Suggestions
+			</div>
 		</div>
 	)
 }
